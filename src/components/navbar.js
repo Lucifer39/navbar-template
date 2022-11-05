5f51acffb4aec741cdc609e8f9a4c95f5adca462
Dipanjan Sarkar <dipanjan@krishworks.com>
2022-11-05 16:56:06 +0530

    responsive

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useState } from "react";
 import { SiFacebook, SiInstagram, SiTwitter, SiLinkedin } from "react-icons/si";
+import { FaBars } from "react-icons/fa";
 import logo from "../assets/logo.png";
 const Navbar = () => {
   const [links, setLinks] = useState(["Home", "About", "Projects", "Contact", "Profile"]);
@@ -11,20 +12,43 @@ const Navbar = () => {
     <SiLinkedin />,
   ]);
 
+  const [navOpen, setNavOpen] = useState(false);
+
+  const linkRef = useRef(null);
+
+  useEffect(() => {
+    if (navOpen) {
+      linkRef.current.style.top = "5rem";
+    } else {
+      linkRef.current.style.top = "-50rem";
+    }
+  }, [navOpen]);
+
   return (
     <div className="navbar">
       <img src={logo} />
-      <ul>
-        {links.map((page) => {
-          return (
-            <li>
-              <a href="#">{page}</a>
-            </li>
-          );
-        })}
-      </ul>
+      <button
+        className="hamburger"
+        onClick={() => {
+          setNavOpen(!navOpen);
+          console.log("clicked");
+        }}
+      >
+        <FaBars />
+      </button>
+      <div className="linkContainer" ref={linkRef}>
+        <ul className="links">
+          {links.map((page) => {
+            return (
+              <li className="options">
+                <a href="#">{page}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
 
-      <ul>
+      <ul className="socials">
         {socials.map((app) => {
           return (
             <li>
